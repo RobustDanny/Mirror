afcbc2461775e9df0060a930cdc6bec275909fb7
RobustDanny <deeabusal@gmail.com>
2026-05-13 21:01:48 -0500

    Stage #1.0.0

---
+++ modules/cryptoShowcase/cryptoShowcase.js
@@ -0,0 +1,65 @@
+Module.register("cryptoShowcase", {
+
+  getStyles() {
+    return ["cryptoShowcase.css"]
+  },
+// Hey
+  start() {
+    this.symbols_data = []
+    this.loaded = false
+  
+    this.sendSocketNotification("CRYPTO_INIT", "BTC + XRP + RED + SIGN + ONDO + ATOM")
+  
+    setInterval(() => {
+      this.sendSocketNotification("CRYPTO_INIT", "BTC + XRP + RED + SIGN + ONDO + ATOM")
+    }, 30000)
+  },
+
+  socketNotificationReceived(notification, payload) {
+    if (notification === "CRYPTO_DATA") {
+      this.symbols_data = payload
+      this.loaded = true
+      this.updateDom()
+    }
+
+    if (notification === "CRYPTO_ERROR") {
+      console.error("Crypto error:", payload)
+    }
+  },
+
+  getDom() {
+    const wrapper = document.createElement("div")
+    wrapper.className = "cards_mesh"
+  
+    if (!this.loaded) {
+      wrapper.innerHTML = "Loading crypto data..."
+      return wrapper
+    }
+  
+    if (!this.symbols_data.length) {
+      wrapper.innerHTML = "No data received"
+      return wrapper
+    }
+  
+    for (const item of this.symbols_data) {
+      const row = document.createElement("div")
+      row.className = "card_container"
+  
+      row.innerHTML = `
+        <div class="stats shadow">
+          <div class="stat">
+            <div class="stat-title">${item.symbol}</div>
+            <div class="stat-value">${item.last_price}</div>
+            <div class="stat-desc ${item.daily_change < 0 ? 'text-red-500' : 'text-green-500'}">
+              ${item.daily_change}
+            </div>
+          </div>
+        </div>
+      `
+  
+      wrapper.appendChild(row)
+    }
+  
+    return wrapper
+  }
+})
---
+++ modules/cryptoShowcase/node_helper.js
@@ -0,0 +1,41 @@
+const NodeHelper = require("node_helper")
+const { FreeCryptoAPI } = require("./freecryptoapi.js")
+
+const api = new FreeCryptoAPI("kp1sg3ome55ari30s62z")
+
+module.exports = NodeHelper.create({
+
+  start() {
+    console.log("CryptoShowcase node_helper started")
+  },
+
+  socketNotificationReceived(notification, payload) {
+    console.log("NODE HELPER GOT:", notification, payload)
+    if (notification === "CRYPTO_INIT") {
+      this.fetchData(payload)
+    }
+  },
+
+  async fetchData(symbol) {
+    try {
+      const data = await api.getData(symbol)
+
+      if (!data.symbols) {
+        this.sendSocketNotification("CRYPTO_ERROR", "No symbols")
+        return
+      }
+
+      const formatted = data.symbols.map(s => ({
+        symbol: s.symbol,
+        last_price: s.last,
+        daily_change: s.daily_change_percentage
+      }))
+
+      this.sendSocketNotification("CRYPTO_DATA", formatted)
+
+    } catch (err) {
+      console.error(err)
+      this.sendSocketNotification("CRYPTO_ERROR", err.message)
+    }
+  }
+})
